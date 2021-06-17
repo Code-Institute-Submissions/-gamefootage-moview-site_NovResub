9bddecd9ad17ad9b7c582f49133fa90095696461
Ben Kelly <benskelly10@gmail.com>
2021-06-17 23:28:25 +0100

    Fix password length validation

--- static/js/register.js
+++ static/js/register.js
@@ -1,8 +1,10 @@
 $("#password, #confirm-password").on("keyup", function(e) {
   if ($("#password").val() !== $("#confirm-password").val()) {
     $("#confirm-password").get(0).setCustomValidity('Password fields must match');
-  } else if ($("#password").val().length > 5 && $("#confirm-password").val().length > 5) {
+  } else if ($("#password").val().length > 4 && $("#confirm-password").val().length > 4) {
     $("#confirm-password").get(0).setCustomValidity('');
+  } else {
+    $("#password").get(0).setCustomValidity('Must be 5 characters in length'); 
   }
 });
 
